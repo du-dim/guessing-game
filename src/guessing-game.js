ae7cd561ee4c4d691ed88d494999eb780bae3213
du-dim <dim-dubovik@yandex.ru>
2021-08-22 22:25:38 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.max = max;
+        this.min = min;
+        this.num = Math.ceil((this.max + this.min)/2);
     }
 
-    guess() {
-
+    guess() {        
+        return this.num;
     }
 
-    lower() {
-
+    lower() {  
+        this.setRange(this.min, this.num);            
     }
 
     greater() {
-
+        this.setRange(this.num, this.max);           
     }
 }
 
